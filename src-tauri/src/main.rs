b325964077e78a3e4a5bcc72a5afea978d00754a
Martin Gressler <martin.gressler@code.berlin>
2024-06-13 13:35:10 -0500

    initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,90 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde::{Deserialize, Serialize};
+use std::{fmt::format, fs::File};
+use sysinfo::{Disks, System};
+use tar::Archive;
+use tauri::api::path;
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            find_skytraxx_mountpoint,
+            extract_transfer_tar
+        ])
+        // .setup(|app| {
+        // })
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}!", name)
+}
+
+#[tauri::command]
+fn find_skytraxx_mountpoint() -> String {
+    let mut sys = System::new_all();
+    sys.refresh_all();
+
+    let disks = Disks::new_with_refreshed_list();
+    let mut mountpoint = String::new();
+
+    for disk in disks.iter() {
+        if disk.name().eq_ignore_ascii_case("skytraxx") {
+            match disk.mount_point().to_str() {
+                None => (),
+                Some(mount) => {
+                    mountpoint = mount.to_string();
+                }
+            }
+        }
+    }
+
+    mountpoint
+}
+
+#[tauri::command]
+fn extract_transfer_tar(tar_path: &str, mountpoint: &str) -> FrontendResult {
+    let download_dir = path::download_dir().unwrap();
+    let f = match File::open(format!("{}/{}", download_dir.to_str().unwrap(), tar_path)) {
+        Ok(f) => f,
+        Err(e) => return FrontendResult::error(e.to_string()),
+    };
+    let mut ar = Archive::new(f);
+
+    match ar.unpack(format!("{mountpoint}/update")) {
+        Ok(_) => (),
+        Err(e) => return FrontendResult::error(e.to_string()),
+    }
+
+    FrontendResult::result("Tar extracted successfully".to_string())
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FrontendResult {
+    error: String,
+    result: String,
+}
+
+impl FrontendResult {
+    fn new(error: String, result: String) -> Self {
+        FrontendResult { error, result }
+    }
+
+    fn error(error: String) -> Self {
+        FrontendResult {
+            error,
+            result: String::new(),
+        }
+    }
+
+    fn result(result: String) -> Self {
+        FrontendResult {
+            error: String::new(),
+            result,
+        }
+    }
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from "@nextui-org/theme";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
@@ -5,16 +6,14 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|ripple|spinner).js",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    extend: {},
+    fontFamily: {
+      skytraxx: ["Skytraxx", "sans-serif"],
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
